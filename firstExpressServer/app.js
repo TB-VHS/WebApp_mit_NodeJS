a0ed644f2cd493264ab7f40a0bdd39a28abf8298
TB-VHS <tb.vhs.e@gmail.com>
2019-11-08 07:00:43 +0100

    Mit session

--- firstExpressServer/app.js
+++ firstExpressServer/app.js
@@ -8,6 +8,8 @@ const express = require( 'express' )
 const app = express()
 const hbs = require('express-handlebars')
 const bodyParser = require( 'body-parser' )
+var session = require( 'express-session' )
+
 const port = 3000
 
 var userData = require( './lib/user_data' )
@@ -21,10 +23,16 @@ app.use( bodyParser.urlencoded({ extended: true }))
 app.engine('handlebars', hbs())
 app.set('view engine', 'handlebars')
 
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  // cookie: { secure: true } <------ hier lag der Hase im Pfeffer! ------- ;)
+}))
 
 /* --- routes --- */
 app.get( '/', ( req, res )=>{
-  res.send( 'Hello World!' )
+  res.redirect( '/login' )
 })
 
 app.get( '/user', ( req, res )=>{
@@ -39,6 +47,7 @@ app.post( '/login', ( req, res )=>{
   // check username & password
   util.log(  'body:',  util.inspect( req.body ))
   if( req.body.first_name + ' ' + req.body.last_name == userData.displayName ){
+    req.session.current_user = { 'authenticated': true, 'name': 'Tom' }
     res.redirect( '/content' )
   } else {
     res.redirect( '/' )
@@ -46,6 +55,7 @@ app.post( '/login', ( req, res )=>{
 })
 
 app.get( '/content', ( req, res )=>{
+  util.log( util.inspect( req.session.current_user ))
   res.render( 'content', { title: 'Content' } )
 })
 
