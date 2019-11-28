d34d4c0361ff93ac353d765fb6dbfa2d61a7dda5
TB-VHS <tb.vhs.e@gmail.com>
2019-11-28 11:23:30 +0100

    calender-test okay

--- firstExpressServer/calendar-test.js
+++ firstExpressServer/calendar-test.js
@@ -2,8 +2,36 @@
 
 const util = require( 'util' )
 const calendar = require( 'node-calendar' )
+const _ = require( 'lodash' )
 
 
+function kalenderMonat( year, month, verbose=false ){
+  var cal = new calendar.Calendar( 0 ).monthdayscalendar( year, month )
+  if( verbose ) util.log( '----- monthdayscalendar -----' )
+  if( verbose ) util.log( cal )
+  if( verbose ) util.log( '----- flatten -----' )
+  cal = _.flatten( cal )
+  if( verbose ) util.log( cal )
+  if( verbose ) util.log( '----- map in object -----' )
+  cal = cal.map( function( dayNumber ){
+    var calDay = new Date( nowYear, nowMonth - 1, dayNumber )
+    ,   calTag = calDay.toLocaleDateString( 'de-DE', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })
+    ,   calArr = calTag.split( ' ' )
+
+    return  {
+              epoche:    calDay.getTime()
+            , calTag:   ( dayNumber === 0 ) ? null :  { wochentag:  _.trimEnd(calArr[ 0 ], ',' )
+                                                      , tageszahl:  _.trimEnd(calArr[ 1 ], '.' )
+                                                      , monatsname: calArr[ 2 ]
+                                                      , jahreszahl: calArr[ 3 ]
+                                                      }
+            }
+  })
+  if( verbose ) util.log( cal )
+  if( verbose ) util.log( '----- done -----' )
+
+  return cal
+}
 
 
 var now = new Date()
@@ -15,6 +43,8 @@ util.log( 'nowMonth:',  nowMonth )
 var nowYear = now.getFullYear()
 util.log( 'nowYear:',  nowYear )
 
-var cal = new calendar.Calendar( 0 ).monthdayscalendar( nowYear, nowMonth )
-cal = cal.flat()
-util.log( cal )
+// var monat = kalenderMonat( nowYear, nowMonth, true )
+var monat = kalenderMonat( nowYear, nowMonth, false )
+util.log( 'return value of kalenderMonat:', util.inspect( monat ))
+
+
