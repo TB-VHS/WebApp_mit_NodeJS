83d63a6af519d3526b1519947827d32b880cfcb1
TB-VHS <tb.vhs.e@gmail.com>
2019-12-05 18:29:05 +0100

    Session 5. Dezember - vorher

--- firstExpressServer/app.js
+++ firstExpressServer/app.js
@@ -11,6 +11,8 @@ const hbs = require('express-handlebars')
 const bodyParser = require( 'body-parser' )
 var session = require( 'express-session' )
 
+const _ = require( 'lodash' )
+
 const port = 3000
 ,     conf = { dbName: 'callipro' }
 // require( 'dotenv' ).config()
@@ -99,8 +101,6 @@ app.get( '/kalender', ( req, res )=>{
   util.log( util.inspect( req.session.currentUser ))
   var dateNow     = new Date()
   ,   monatJetzt  = dateNow.toLocaleDateString( 'de-DE', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }).split(' ')[ 2 ]
-
-
   let db = new sqlite3.Database( `./db/${ conf.dbName }.db`, sqlite3.OPEN_READWRITE, ( err )=>{
     if( err ){ console.error( err.message )}
     console.log( `Connected to the ${ conf.dbName } database.` )
@@ -113,12 +113,55 @@ app.get( '/kalender', ( req, res )=>{
               , { title:        'callipro'
                 , monatJetzt:   monatJetzt
                 , displayName:  req.session.currentUser.displayName
-                , kalenderTage: helpers.kalenderMonat( dateNow.getFullYear(), dateNow.getMonth() + 1, allRows )
+                , kalenderTage: helpers.kalenderMonat( dateNow.getFullYear(), dateNow.getMonth() + 1, allRows, true )
                 })
   })
 
 })
 
+app.post( '/kalender_tag', ( req, res )=>{
+  util.log( 'ACTION:', req.body.action )
+  var db = new sqlite3.Database( `./db/${ conf.dbName }.db`, sqlite3.OPEN_READWRITE, ( err )=>{
+    if( err ){ console.error( err.message )}
+    console.log( `Connected to the ${ conf.dbName } database.` )
+  })
+  switch( req.body.action ){
 
+    case 'retrieve':
+      var datumArr = req.body.datum.split( '-' )
+      ,   datum = { 
+                    tageszahl:   datumArr[ 2 ]
+                  , monatszahl:  datumArr[ 1 ] - 1
+                  , jahreszahl:  datumArr[ 0 ]
+                  , iso8601:     req.body.datum
+                  }
+      db.all( `SELECT * FROM dates WHERE year='${ datum.jahreszahl }' AND month='${ _.trimStart( datum.monatszahl, '0' )}' AND day='${ _.trimStart( datum.tageszahl, '0' )}'`, ( err, allRows )=>{
+        if( err ){ console.error( err.message )}
+    // Datensatz verarbeiten --->
+    util.log( 'DATUM:', util.inspect( datum ))
+    util.log( `allRows_kalender_tag: ${ util.inspect( allRows )}` )
+        res.render( 'kalender_tag'
+                  , { title:   ( new Date( datum.iso8601.split( '-' ))).toLocaleDateString( 'de-DE', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })
+                    , dates:   allRows
+                    })
+      })
+      break
+   
+    case 'create':
+      db.run( `INSERT INTO ${ conf.tableName }( id, year, month, day, hour, minute, subject, created_at, created_by ) VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ? )`
+            , req.body.date
+            , function( err ){
+                if( err ){ return console.log( err.message )}
+        // get the last inserted id
+                util.log( `A row has been inserted with rowid ${ this.lastID }` )
+              })
+      break
+
+    default:
+      util.log( 'not supported action:', req.body.action )
+  }
+
+  db.close()
+})
 /* --- server start --- */
-app.listen( port, () => console.log( `Example app listening on port ${port}!` ))
+app.listen( port, () => console.log( `Example app listening on port ${ port }!` ))
--- firstExpressServer/lib/app_helpers.js
+++ firstExpressServer/lib/app_helpers.js
@@ -8,21 +8,34 @@ const _ = require( 'lodash' )
 module.exports = {
 
   kalenderMonat:  ( year, month, datesRows, verbose=false )=>{
+                    
                     var cal = new calendar.Calendar( 0 ).monthdayscalendar( year, month )
                     if( verbose ) util.log( '----- monthdayscalendar -----' )
                     if( verbose ) util.log( cal )
                     if( verbose ) util.log( '----- flatten -----' )
+
                     cal = _.flatten( cal )
                     if( verbose ) util.log( cal )
                     if( verbose ) util.log( '----- map in object -----' )
-                    cal = cal.map( function( dayNumber ){
+
+                    cal = cal.map(( dayNumber )=>{
+                      if( verbose ) util.log( 'map dayNumber', dayNumber )
+
                       var calDate   = new Date( year, month - 1, dayNumber, 3 )
                       ,   calTag    = calDate.toLocaleDateString( 'de-DE', { weekday: 'short', year: 'numeric', month: 'long', day: 'numeric' })
                       ,   calArr    = calTag.split( ' ' )
+                      if( verbose ) util.log( 'calDate:', calDate )
+                      if( verbose ) util.log( 'calTag:', calTag )
 
-                      var   dates     = []
-                      datesRows.forEach(( row ) => {
+                      var dates = []
+                      if( verbose ) util.log( 'datesRows:\n', util.inspect( datesRows ))
+                      
+                      datesRows.forEach(( row )=>{
+                        if( verbose ) util.log( 'row:', util.inspect( row ))
+                        if( verbose ) util.log( 'dayNumber:', dayNumber )
+
                         if( parseInt( row.day ) === dayNumber ){
+                          if( verbose ) util.log( 'MATCH')
                           dates.push({  epoche:   row.id
                                       , hour:     _.padStart( row.hour, 2, '0' )
                                       , minute:   _.padStart( row.minute, 2, '0' )
@@ -30,16 +43,23 @@ module.exports = {
                                       })
                         }
                       })
+                      if( verbose ) util.log( 'dates:\n', util.inspect( dates ))
 
-                      return  {
-                                epoche:   calDate.getTime()
-                              , calTag:   ( dayNumber === 0 ) ? null :  { wochentag:  _.trimEnd(calArr[ 0 ], '.,' )
-                                                                        , tageszahl:  _.trimEnd(calArr[ 1 ], '.' )
-                                                                        , monatsname: calArr[ 2 ]
-                                                                        , jahreszahl: calArr[ 3 ]
-                                                                        }
-                              , dates:    ( dates.length > 0 ) ? dates : null
-                              }
+                      var dateInfo =  {
+                                        epoche:   calDate.getTime()
+                                      , calTag:   ( dayNumber === 0 ) ? null :  { wochentag:  _.trimEnd(calArr[ 0 ], '.,' )
+                                                                                , tageszahl:  _.trimEnd(calArr[ 1 ], '.' )
+                                                                                , monatsname: calArr[ 2 ]
+                                                                                , jahreszahl: calArr[ 3 ]
+                                                                                }
+                                      , dates:    ( dates.length > 0 ) ? dates : null
+                                      }
+                      if( verbose ) util.log( 'dateInfo:\n', util.inspect( dateInfo ))
+
+                      if( dateInfo.calTag !== null ){
+                        dateInfo.datum = `${ year }-${ _.padStart( month, 2, '0' )}-${ _.padStart( dateInfo.calTag.tageszahl, 2, '0' )}`
+                      }
+                      return dateInfo
                     })
                     if( verbose ) util.log(util.inspect( cal, false, null ))
                     if( verbose ) util.log( '----- done -----' )
